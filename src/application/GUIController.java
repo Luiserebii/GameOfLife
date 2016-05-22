a0ae0ec121163946dd45542fe4b6eda503df7633
Luiserebii <luis@serebii.io>
2016-05-21 23:17:27 -0400

    GUIController set up, program now initializes into GUI prompt. Needs further testing

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -2,11 +2,17 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class GUIController implements Initializable {
 
@@ -22,12 +28,55 @@ public class GUIController implements Initializable {
 
 	}
 
+	@FXML
+	private void startGameOfLife(){
+
+		int sL = getLength();
+		int sW = getWidth();
+
+		if(sL == 90000 || sW == 90000){
+			messageLabel.setText("Length or width is not a number; check input");
+		}
+		if(sL < 1 && sW < 1){
+			messageLabel.setText("Number in field cannot be negative or non-zero");
+		}
+
+		Stage s = new Stage();
+		Group root = new Group();
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		s.setScene(scene);
+		s.setTitle("The Game of Life");
+
+		GameOfLifeLogic gll = new GameOfLifeLogic(sL,sW);
+		Canvas canvas = new Canvas(sL,sW); //600,400
+		root.getChildren().add(canvas);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		GameOfLifeLoop gl = new GameOfLifeLoop(gc,gll);
+		gl.setup();
+		gl.start();
+
+		s.show();
+
+	}
+
 	public int getLength(){
-		return Integer.valueOf(lengthField.getText());
+		int l;
+
+		try {
+			l = Integer.valueOf(lengthField.getText());
+		} catch (Exception e){
+			l = 90000; //No one will use this number, so it should be safe to use
+		}
+
+		return l;
 	}
 
 	public int getWidth(){
 		return Integer.valueOf(widthField.getText());
 	}
+
+
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -3,8 +3,10 @@ package application;
 import java.util.Scanner;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -15,29 +17,37 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Group root = new Group();
+//			Group root = new Group();
+//			Scene scene = new Scene(root);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.setTitle("The Game of Life");
+//
+//			Scanner sc = new Scanner(System.in);
+//			System.out.println("Enter Length and Width");
+//			int sL = sc.nextInt();
+//			int sW = sc.nextInt();
+//			GameOfLifeLogic gll = new GameOfLifeLogic(sL,sW);
+//
+//			Canvas canvas = new Canvas(sL,sW); //600,400
+//			root.getChildren().add(canvas);
+//
+//			GraphicsContext gc = canvas.getGraphicsContext2D();
+//
+//			GameOfLifeLoop gl = new GameOfLifeLoop(gc,gll);
+//			gl.setup();
+//			gl.start();
+//
+//
+//			primaryStage.show();
+
+			Parent root = FXMLLoader.load(getClass().getResource("../view/GUI.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("The Game of Life");
-
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter Length and Width");
-			int sL = sc.nextInt();
-			int sW = sc.nextInt();
-			GameOfLifeLogic gll = new GameOfLifeLogic(sL,sW);
-
-			Canvas canvas = new Canvas(sL,sW); //600,400
-			root.getChildren().add(canvas);
-
-			GraphicsContext gc = canvas.getGraphicsContext2D();
-
-			GameOfLifeLoop gl = new GameOfLifeLoop(gc,gll);
-			gl.setup();
-			gl.start();
-
-
+			primaryStage.setTitle("Game of Life Settings");
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
