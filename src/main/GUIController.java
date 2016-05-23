b5a8be915812ded5f4b88e340459d71f6aec194a
Luiserebii <luis@serebii.io>
2016-05-23 19:32:30 -0400

    getLength() and getWidth() methods re-written for better practice, now throws Exception instead of attempting to handle it. Unused imports removed.

--- src/main/GUIController.java
+++ src/main/GUIController.java
@@ -3,7 +3,6 @@ package main;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 import gameoflife.GameOfLifeLogic;
 import gameoflife.GameOfLifeLogic.LogicBuilder;
@@ -71,35 +70,38 @@ public class GUIController implements Initializable {
 	@FXML
 	private void startGameOfLife(){
 
-		int sL = getLength();
-		int sW = getWidth();
+		int sL, sW;
+		try {
+			sL = getLength();
+			sW = getWidth();
 
-		if(sL == 90000 || sW == 90000){
+			if(sL < 1 || sW < 1){
+				messageLabel.setText("Number in field cannot be negative or non-zero");
+			} else {
+
+				Stage s = new Stage();
+				Group root = new Group();
+				Scene scene = new Scene(root);
+				scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
+				s.setScene(scene);
+				s.setTitle("The Game of Life");
+
+				GameOfLifeLogic gll = new GameOfLifeLogic(sL,sW,logicbuilder);
+				Canvas canvas = new Canvas(sL,sW); //600,400
+				root.getChildren().add(canvas);
+				GraphicsContext gc = canvas.getGraphicsContext2D();
+
+				GameOfLifeLoop gl = new GameOfLifeLoop(gc, gll, loopbuilder);
+				s.setOnCloseRequest(event -> {
+				    gl.setIsRunning(false);
+				});
+				gl.setup();
+				gl.start();
+
+				s.show();
+			}
+		} catch(NumberFormatException e){
 			messageLabel.setText("Length or width is not a number; check input");
-		} else if(sL < 1 || sW < 1){
-			messageLabel.setText("Number in field cannot be negative or non-zero");
-		} else {
-
-			Stage s = new Stage();
-			Group root = new Group();
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
-			s.setScene(scene);
-			s.setTitle("The Game of Life");
-
-			GameOfLifeLogic gll = new GameOfLifeLogic(sL,sW,logicbuilder);
-			Canvas canvas = new Canvas(sL,sW); //600,400
-			root.getChildren().add(canvas);
-			GraphicsContext gc = canvas.getGraphicsContext2D();
-
-			GameOfLifeLoop gl = new GameOfLifeLoop(gc, gll, loopbuilder);
-			s.setOnCloseRequest(event -> {
-			    gl.setIsRunning(false);
-			});
-			gl.setup();
-			gl.start();
-
-			s.show();
 		}
 
 	}
@@ -165,24 +167,12 @@ public class GUIController implements Initializable {
 		}
 	}
 
-	public int getLength(){
-		int l;
-		try {
-			l = Integer.valueOf(lengthField.getText());
-		} catch(Exception e){
-			l = 90000; //No one will use this number, so it should be safe to use
-		}
-		return l;
+	public int getLength() throws NumberFormatException{
+		return Integer.valueOf(lengthField.getText());
 	}
 
-	public int getWidth(){
-		int w;
-		try {
-			w = Integer.valueOf(widthField.getText());
-		} catch(Exception e){
-			w = 90000; //No one will use this number, so it should be safe to use
-		}
-		return w;
+	public int getWidth() throws NumberFormatException{
+		return Integer.valueOf(widthField.getText());
 	}
 
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,10 @@
 package main;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
